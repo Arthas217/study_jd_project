50337084b4401bd427b139ad97654e1151fb67ec
zuocheng <442854399@qq.com>
2021-12-19 15:32:15 +0800

    Initial commit

---
+++ src/main/java/com/jd/study/demo/Study20211219Application.java
@@ -0,0 +1,13 @@
+package com.jd.study.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study20211219Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Study20211219Application.class, args);
+    }
+
+}
